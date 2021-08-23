59161312b3e0c97aebcde8ef6b3b7843853765ea
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-08-23 08:33:17 +0000

    Improve test readability

--- meta-k8s-testing/lib/oeqa/runtime/cases/kubeadm.py
+++ meta-k8s-testing/lib/oeqa/runtime/cases/kubeadm.py
@@ -8,11 +8,11 @@ from os.path import isfile, join
 
 class KubeadmInitTest(OERuntimeTestCase):
 
-    def test_kubeadm_init_preflight(self):
-
+    def test_kubeadm_init(self):
+        # Load container images for control-plane 
         (status, output) = self.target.run(f"podman load -i /images.tar")
         self.assertEqual(status, 0, msg=output)
-
+        # Create the cluster
         (status, output) = self.target.run('kubeadm init --config /InitConfiguration.yaml')
         self.assertEqual(status, 0, msg=output)
 
