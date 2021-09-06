26d31435bcbc61d053e8c7a36bad3f4fac156c4e
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-09-06 15:04:41 +0000

    Using grep to run test.

--- meta-k8s-setup/lib/oeqa/runtime/cases/config.py
+++ meta-k8s-setup/lib/oeqa/runtime/cases/config.py
@@ -8,7 +8,6 @@ from os.path import isfile, join
 class ConfigPartitionTest(OERuntimeTestCase):
 
     def test_config_present(self):
-        # Check to see that the config tar is correctly mounted
-        self.assertTrue(os.path.exists("/config/config.tar.gz"))
-        # Count from grep should be 1
+        (status, output) = self.target.run(f"ls /config | grep config.tar.gz -c")
+        self.assertEqual(status, 0, msg=output)
         self.assertEqual(output, "1", msg=output)
